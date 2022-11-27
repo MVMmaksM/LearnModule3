b7448764131a1287d5e1414c735b107a37720c66
MVMmaksM <vaganov.maksim@mail.ru>
2022-11-27 17:15:19 +0500

    Табуляция, перенос тсроки, вывод симоволв Unicode

--- LearnModule3/Program.cs
+++ LearnModule3/Program.cs
@@ -4,25 +4,16 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        //const string MyName = "Maks";
+        const string MyName = "Maks";
 
-        //Console.WriteLine(MyName);
+        Console.WriteLine(MyName);
 
-        //Console.WriteLine("\t Привет Мир");
-        //Console.WriteLine("\t Мне 27 лет");
-        //Console.WriteLine("\t My name is \n {0}", MyName);
-        //Console.WriteLine("\u0040");
-        //Console.WriteLine("\u0023");
-        //Console.ReadKey();
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\u0023");
+        Console.ReadKey();
 
-        var num = 1;
-        AddTen(ref num);
-        Console.WriteLine(num);
-
-    }
-
-    static void AddTen(ref int num) 
-    {
-        num = num + 10;
-    }
+    }  
 }
