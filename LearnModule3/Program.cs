e2df578534a890bcddf8c6b2cf3b498cd76eb04b
MVMmaksM <vaganov.maksim@mail.ru>
2022-11-27 17:15:04 +0500

    Табуляция, перенос тсроки, вывод симоволв Unicode

--- LearnModule3/Program.cs
+++ LearnModule3/Program.cs
@@ -4,12 +4,25 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        //const string MyName = "Maks";
 
-        string myName;
-        myName = "Maksim";
+        //Console.WriteLine(MyName);
 
-        Console.WriteLine(myName);
+        //Console.WriteLine("\t Привет Мир");
+        //Console.WriteLine("\t Мне 27 лет");
+        //Console.WriteLine("\t My name is \n {0}", MyName);
+        //Console.WriteLine("\u0040");
+        //Console.WriteLine("\u0023");
+        //Console.ReadKey();
 
-        Console.ReadKey();
+        var num = 1;
+        AddTen(ref num);
+        Console.WriteLine(num);
+
+    }
+
+    static void AddTen(ref int num) 
+    {
+        num = num + 10;
     }
 }
